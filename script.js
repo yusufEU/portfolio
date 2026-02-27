2b5e1b09ac43b2857255e3dc9eb15d7043bef5f4
YUE <125772117+yusufEU@users.noreply.github.com>
2026-02-27 22:32:43 +0300

    изменена форма

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const form = document.querySelector('#form');
+const message = document.querySelector('#form-message');
+
+const username = document.querySelector('#username');
+const msg = document.querySelector('#msg');
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault(); // стоп перезагрузка
-  alert('Спасибо');
+  event.preventDefault();
+
+  const nameValue = username.value.trim();
+  const msgValue = msg.value.trim();
+
+  if (nameValue === '' || msgValue === '') {
+    message.textContent = 'Заполни имя и сообщение.';
+    message.style.color = 'red';
+    return;
+  }
+
+  message.textContent = 'Спасибо! Сообщение отправлено.';
+  message.style.color = 'green';
+
+  form.reset();
 });
